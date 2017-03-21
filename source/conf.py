16fbf73c79a9631ec3bb02c5761dd903eececfd2
KickWood <tkgame001@gmail.com>
2017-03-21 13:33:59 +0100

    Configuring theme

--- source/conf.py
+++ source/conf.py
@@ -91,7 +91,10 @@ extensions.append("guzzle_sphinx_theme")
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    # Set the name of the project to appear in the sidebar
+    "project_nav_name": "Magister 6 API",    
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
